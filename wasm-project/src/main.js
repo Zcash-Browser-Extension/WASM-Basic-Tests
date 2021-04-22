2de7c3c031fafa8f633eaa62c162751fe6f37d4f
Elliot Blanchard <git@elliotblanchard.com>
2021-04-22 09:37:19 -0400

    Code working in web page

--- wasm-project/src/main.js
+++ wasm-project/src/main.js
@@ -1,26 +1,16 @@
 (async function fetchAndInstantiate() {
-  const response = await fetch('../out/main.wasm');
-  const buffer = await response.arrayBuffer();
-  const obj = await WebAssembly.instantiate(buffer);
-  console.log("Hello")
-  console.log(obj.instance.exports.helloWorld());
-})()
+  const response = await fetch('../out/main.wasm')
+  const buffer = await response.arrayBuffer()
+  const obj = await WebAssembly.instantiate(buffer)
+  const output = new Uint8Array(obj.instance.exports.memory.buffer)
 
-/*
-(async () => {
-  const codePromise = fetch('../out/main.wasm')
-  const { instance } = await WebAssembly.instantiate(codePromise)
-  
-  const buffer = new Uint8Array(instance.exports.memory.buffer)
-
-  const pointer = instance.exports.helloWorld()
+  const pointer = obj.instance.exports.helloWorld()
 
   let string = ""
 
-  for(let i = pointer; buffer[i]; i++) {
-    string += String.fromCharCode(buffer[i])
-  }
+  for(let i = pointer; output[i]; i++) {
+    string += String.fromCharCode(output[i])
+  }  
 
   document.getElementById('container').textContent = string
 })()
-*/
